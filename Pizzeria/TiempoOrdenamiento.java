5e6fe8121810ee25a1af024807b84744847d475c
tomas-calvo01 <tcalvo246@alumnos.iua.edu.ar>
2024-09-05 08:24:02 -0300

    .

--- Pizzeria/TiempoOrdenamiento.java
+++
@@ -1,8 +0,0 @@
-public class TiempoOrdenamiento {
-
-    public static long medirTiempo(Runnable algoritmo) {
-        long start = System.nanoTime();
-        algoritmo.run();
-        return System.nanoTime() - start;
-    }
-}
